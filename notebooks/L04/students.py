8bc92e71cf766d51d246ce6684be383689696fb0
Dieter Probst <dieter.probst@gmail.com>
2024-11-18 14:36:32 +0100

    .

--- notebooks/L04/students.py
+++
@@ -1,9 +0,0 @@
-students = [
-    ('Mike', 'Blättler', 'mike.blaettler@edu.teko.ch'),
-    ('Antonio', 'Fernandes', 'antonio.fernandes@edu.teko.ch'),
-    ('Steve', 'Kluser', 'steve.kluser@edu.teko.ch'),
-    ('Eric', 'Lechmann', 'eric.lechmann@edu.teko.ch'),
-    ('Jean', 'Leutwyler', 'jean.leutwyler@edu.teko.ch'),
-    ('Steve', 'Kluser', 'steve.kluser@edu.teko.ch'),
-    ('Marko', 'Stamenkovic', 'marko.stamenkovic@edu.teko.ch'),
-]
